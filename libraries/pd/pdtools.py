d9405fb4381cf9c2678dcb511a693d4edb71463b
André Oliveira <andrehosantos@gmail.com>
2023-05-20 22:51:09 -0300

    moved 'get_files' method to utils.FileHandler

--- libraries/pd/pdtools.py
+++ libraries/pd/pdtools.py
@@ -20,22 +20,6 @@ class Dataframe:
         self.dataframes = {}
         self.concat_dataframes = {}
 
-    @staticmethod
-    def get_files(dir: str) -> list:
-        """
-        Retrieve a list of files in a directory and its subdirectories.
-
-        Args:
-            dir (str): The directory path.
-
-        Returns:
-            list: A list of file paths.
-        """
-        files_list = []
-        for root, _, files in os.walk(dir):
-            files_list.extend([os.path.join(root, file) for file in files])
-        return files_list
-
     def import_data(self, files_list: list) -> list:
         """
         Import data from a list of file paths.
--- utils/FileHandler.py
+++ utils/FileHandler.py
@@ -1,42 +1,15 @@
 import os
 
-
-def get_files(folder_path: str = './', ext: str = '.txt') -> tuple:
+@staticmethod
+def get_files(dir: str) -> list:
     """
-    Return a dictionary of all filenames and their corresponding paths given a folder name to look into.
-
+    Retrieve a list of files in a directory and its subdirectories.
     Args:
-        folder_path (str): relative folder path (default='./')
-
+        dir (str): The directory path.
     Returns:
-        dictionary: A nested dictionary of the form {unit: {concentration: [list of file paths]}}
+        list: A list of file paths.
     """
-    files_dict = {}
-    for root, dirs, files in os.walk(folder_path):
-        if len(files) > 0:
-            folder_parts = root.split(os.sep)
-            concentration = folder_parts[-1]
-            units = folder_parts[-2]
-            files_list = [os.path.join(root, file) for file in files if file.endswith(ext)]
-            if units not in files_dict: 
-                files_dict[units] = {}
-            if concentration not in files_dict[units]:
-                files_dict[units][concentration] = []
-            files_dict[units][concentration].append(files_list)
-    return files_dict
-
-def groupby_folder(files: tuple) -> tuple:
-    """List all files grouped within the same folder
-
-    Args:
-        files (tuple): a list of all files and folders
-
-    Returns:
-        tuple: a list of files grouped by folders
-    """
-    grouped = {}
-    for concentration, dirpath, filepath in files:
-        if dirpath not in grouped:
-            grouped[dirpath] = []
-        grouped[dirpath].append([filepath, dirpath])
-    return tuple(grouped.items())
+    files_list = []
+    for root, _, files in os.walk(dir):
+        files_list.extend([os.path.join(root, file) for file in files])
+    return files_list
