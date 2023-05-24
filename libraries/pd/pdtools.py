34ca01ee7a9fe63b8837edde946dc8d92ee246f5
André Oliveira <andrehosantos@gmail.com>
2023-05-24 11:31:31 -0300

    remove unnecessary comments and update dosctrings

--- libraries/pd/pdtools.py
+++ libraries/pd/pdtools.py
@@ -14,7 +14,7 @@ class Dataframe:
         Initialize the Dataframe object.
 
         Args:
-            file_list (str): The path to the file or directory.
+            file_list (dict): A dictionary of files organized by units and concentrations.
             sep (str): The delimiter used in the file (default is '\t' for tab-separated files).
         """
         self.file_list = file_list
@@ -33,7 +33,7 @@ class Dataframe:
             list: A list of Pandas DataFrames.
         """
         data = []
-        for file in files_list[0]:
+        for file in files_list:
             data.append(pd.read_csv(file, sep=self.sep))
         return data
 
@@ -50,16 +50,12 @@ class Dataframe:
         """
         return df.drop(df.columns[indices], axis=1)
 
-    def create_dataframes(self, drop_columns: list) -> dict:
+    def create_dataframes(self, drop_columns: list) -> None:
         """
         Create multiple dataframes from a dictionary of files.
 
         Args:
-            files_list (dict): A dictionary of files organized by units and concentrations.
-            drop_indices (list): A list of column indices to drop from the dataframes.
-
-        Returns:
-            dict: A dictionary of dataframes organized by units and concentrations.
+            drop_columns (list): A list of column indices to drop from the dataframes.
         """
         for units, concentrations in self.file_list.items():
             if units not in self.dataframes:
@@ -71,19 +67,13 @@ class Dataframe:
                 if drop_columns:
                     for i, data_frame in enumerate(dfs):
                         data_frame = self.drop_columns(data_frame, drop_columns)
-                        self.dataframes[units][concentration].append(data_frame)  # Append individual DataFrame
+                        self.dataframes[units][concentration].append(data_frame)
                 else:
-                    self.dataframes[units][concentration].extend(dfs)  # Append the entire list of DataFrames
+                    self.dataframes[units][concentration].extend(dfs)
 
-    def concatenate_dataframes(self) -> dict:
+    def concatenate_dataframes(self) -> None:
         """
         Concatenate multiple dataframes.
-
-        Args:
-            pd_dataframes (dict): A dictionary of dataframes organized by units and concentrations.
-
-        Returns:
-            dict: A dictionary of concatenated dataframes organized by units and concentrations.
         """
         for units, concentrations in self.dataframes.items():
             if units not in self.concat_dataframes:
@@ -99,120 +89,35 @@ class Dataframe:
         Get the biggest dataframe from a dictionary of dataframes.
 
         Args:
-            dataframes (dict): A dictionary of dataframes organized by units and concentrations.
+            concatenated (bool): Indicates whether to consider concatenated dataframes.
 
         Returns:
-            pd.DataFrame: The biggest dataframe.
+            dict: A dictionary containing the biggest dataframe for each unit.
         """
-        biggest_shape = None
-        biggest_df = {}
-
-        dataframes = self.dataframes
-        if concatenated:
-            dataframes = self.concat_dataframes
         biggest_dataframes = {}
-    
+
+        dataframes = self.concat_dataframes if concatenated else self.dataframes
+
         for units, concentrations in dataframes.items():
             if units not in biggest_dataframes:
                 biggest_dataframes[units] = None
-            biggest_df = None
-            max_shape = (0,0)
+            max_shape = (0, 0)
             for concentration, dfs in concentrations.items():
                 for df in dfs:
                     if df.shape > max_shape:
                         max_shape = df.shape
                         biggest_dataframes[units] = df
         return biggest_dataframes
-    
+
     def groupdf_by(self, dataframes: dict, column: str) -> dict:
+        """
+        Group dataframes by a specified column.
+
+        Args:
+            dataframes (dict): A dictionary of dataframes organized by units and concentrations.
+            column (str): The column name to group by.
+
+        Returns:
+            dict: A dictionary of grouped dataframes.
+        """
         pass
-
-
-# """
-# This is the original code. It was changed for bugs correction
-# and improvements by GPT-3.5-turbo model using OpenAI API.
-# The docstrings were generated # in the same way.
-# """
-
-# from dataclasses import dataclass
-# import pandas as pd
-    
-
-# class Dataframe(pd.DataFrame):
-#     def __init__(self, file_list: str = './', sep: str = '\t') -> None:
-#         super().__init__()
-#         self.file_list = file_list
-#         self.sep = sep
-#         self.dataframes = {}
-    
-#     def import_data(self, files_list: list) -> list:
-#         data = []
-#         for file in files_list[0]:
-#             data.append(pd.read_csv(file, sep=self.sep))
-#         return data
-    
-#     def drop_columns(self, df: pd.DataFrame, indices: list = []) -> list:
-#         """
-        
-#         """
-#         columns = list(df.columns)
-#         columns_to_drop = [columns[i] for i in indices]
-#         dropped_df = df.drop(columns_to_drop, axis=1, inplace=True)
-#         return dropped_df
-        
-#     def create_dataframes(self, files_list: dict, drop_indices: list) -> dict:
-#         """
-#         Generate a new pandas dataframe from a passed fully-qualified filename
-#         Returns:
-#             dataframe: Two-dimensional table
-#         """
-#         dataframes = {}
-#         for units, concentrations in files_list.items():
-#             if units not in dataframes:
-#                 dataframes[units] = {}
-#             for concentration, files in concentrations.items():
-#                 dfs = self.import_data(files)
-#                 if concentration not in dataframes[units]:
-#                     dataframes[units][concentration] = []
-#                 for df in dfs:
-#                     self.drop_columns(df, drop_indices)
-#                     dataframes[units][concentration].append(df)
-#         return dataframes
-
-#     def concatenate_dataframes(self, pd_dataframes: pd.DataFrame) -> pd.DataFrame:
-#         """Concatenate two or more pandas dataframes
-
-#         Returns:
-#             pd.DataFrame: Concatenated dataframe
-#         """
-#         concat_dataframes = {}
-#         for units, concentrations in pd_dataframes.items():
-#             if units not in concat_dataframes:
-#                 concat_dataframes[units] = {}
-#             for concentration, dfs in concentrations.items():
-#                 if concentration not in concat_dataframes[units]:
-#                     concat_dataframes[units][concentration] = []
-#                 concat_dfs = pd.concat(dfs, axis=0)
-#                 concat_dataframes[units][concentration].append(concat_dfs)
-#         return concat_dataframes
-
-
-#     def concatenate_by_folder(self, files_folders: tuple) -> pd.DataFrame:
-#         """Concatenate multiple files in a folder .
-
-#         [extended_summary]
-
-#         Args:
-#             files_folders (tuple): [description]
-
-#         Returns:
-#             pd.DataFrame: [description]
-#         """
-#         df_list = []
-#         for _, filelist in files_folders:
-#             dataframes = []
-#             for file in filelist:
-#                 dataframe = self.create_dataframe(file)
-#                 dataframes.append(dataframe)
-#             conc_df = self.concatenate_dataframes(dataframes)
-#             df_list.append(conc_df)
