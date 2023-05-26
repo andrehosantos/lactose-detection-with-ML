5de221a5d85b40e2ca973feb2e262da6da04157e
André Oliveira <andrehosantos@gmail.com>
2023-05-26 11:05:12 -0300

    remove unnecessary comments

--- utils/FileHandler.py
+++ utils/FileHandler.py
@@ -1,17 +1,5 @@
 import os
 
-# def get_files(dir: str) -> list:
-#     """
-#     Retrieve a list of files in a directory and its subdirectories.
-#     Args:
-#         dir (str): The directory path.
-#     Returns:
-#         list: A list of file paths.
-#     """
-#     files_list = []
-#     for root, _, files in os.walk(dir):
-#         files_list.extend([os.path.join(root, file) for file in files])
-#     return files_list
 
 def get_files(folder_path: str = './', ext: str = '.txt') -> dict:
     """
